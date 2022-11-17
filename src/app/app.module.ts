0a68cc1ad5ed21e994ab4e9217f262ce77456119
Sergio De Luca <sdlbsso@gmail.com>
2022-11-17 20:52:38 -0300

    sistema de rutas con lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { GraficasModule } from './graficas/graficas.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MenuComponent } from './shared/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraficasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
