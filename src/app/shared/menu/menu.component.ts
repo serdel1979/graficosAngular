9a99a6333f4e70676ea7814fad0439f7a8d8516f
Sergio De Luca <sdlbsso@gmail.com>
2022-11-17 23:06:43 -0300

    armado de menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem{
+  ruta: string;
+  texto: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+
+  `
   ]
 })
 export class MenuComponent implements OnInit {
+
+  menu: MenuItem[]=[
+    {
+      ruta:'/graficos/barra',
+      texto:'Barras',
+    },
+    {
+      ruta:'/graficos/barra-doble',
+      texto:'Barras Dobles',
+    },
+    {
+      ruta:'/graficos/dona',
+      texto:'Dona',
+    },
+    {
+      ruta:'/graficos/dona-http',
+      texto:'Dona Http',
+    }
+  ]
 
   constructor() { }
 
